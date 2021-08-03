975c1ab6ad91c5fcd5317a46c5afa838ad29c4b1
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2021-08-03 20:14:48 +0200

    Add Group entity

---
+++ src/Entity/Group.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GroupRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GroupRepository::class)
+ * @ORM\Table(name="`group`")
+ */
+class Group implements GroupInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $name = null;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $code = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+}
---
+++ src/Entity/GroupInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Entity;
+
+interface GroupInterface
+{
+    public function getId(): ?int;
+
+    public function getName(): ?string;
+
+    public function setName(?string $name): Group;
+
+    public function getCode(): ?string;
+
+    public function setCode(?string $code): Group;
+}
