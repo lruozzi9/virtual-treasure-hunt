5abbadf21eb3005a955adf4b592e32abe9d612a8
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2021-09-20 21:48:08 +0200

    Fix ECS

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -8,7 +8,6 @@ use App\Repository\GameRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
 /**
  * @ORM\Entity(repositoryClass=GameRepository::class)
  */
